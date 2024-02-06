2ff803ff509e6dc402d807667cdc1796cb89b8d5
Daviddairot <133882462+Daviddairot@users.noreply.github.com>
2024-02-06 13:28:34 +0100

    Update urls.py

--- nuproject/urls.py
+++ nuproject/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls, namespace = 'admin'),
+    path('admin/', admin.site.urls, name = 'admin'),
     path('', include("nuapp.urls")),
 ]
